371f1f0f4d380d4bb6998fc813974afc3d96aa41
Mike <mikemorel3322@gmail.com>
2021-03-10 18:54:53 -0600

    Starting Progress Tracker. I can write and create files.
    Ignore all the random crap. progress.cs is an example of a hello world file and you can peep my progresstracker.cs for insperation

---
+++ GOOP Source/ProgressTracker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO; 
+using System.Threading.Tasks;
+
+namespace GOOP
+{
+    class ProgressTracker
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            makeUserAssignments();
+            writeToUserAssignments(); //writing to a file
+        }
+
+        public static async Task writeToUserAssignments()
+        {
+            string[] lines =
+            {
+                "First Asgn", "Second Asgn", "Third Asgn" 
+            };
+            await File.WriteAllLinesAsync(@"UserAssignments.txt", lines);
+        }
+
+        public static async Task makeUserAssignments()
+        {
+            string fileName = @"UserAssignments.txt"; 
+            if (!File.Exists(fileName))    
+            {    
+                File.Create(fileName);    
+            }    
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*using System;
+
+namespace GOOP
+{
+    class Program  //change this to your file name.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+Copy paste for hello world file. run by typing: "dotnet run" into terminal
+Keep in mind that there can be only one Main class, so the main should go in whatever class starts the app I think 
+*/
