2e9ce6ec61ac66fe86fc5aad95e5099c73f3b1ac
atran18 <49043642+atran18@users.noreply.github.com>
2021-03-15 11:09:49 -0500

    Add files via upload
    GUI sample

---
+++ GOOP Source/sample1/UpdateFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace sample1
+{
+    class UpdateFile
+    {
+        public static class Globals
+        {
+            public static string path = @"C:\\Users\\Angel\\Documents\\Spring 2021\\CSC 3380 - OOD\\Code\\sample1\\text\\taskList.txt";
+        }
+        public static void Update(string input)
+        {
+            //string path = @"C:\\Users\\Angel\\Documents\\Spring 2021\\CSC 3380 - OOD\\Code\\sample1\\text\\taskList.txt";
+            if (!File.Exists(Globals.path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(Globals.path))
+                {
+                    sw.WriteLine(input);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(Globals.path))
+                {
+                    sw.WriteLine(input);
+                    //Console.WriteLine("Added: " + input);
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(Globals.path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        public static void TListR()
+        {
+            //string path = @"C:\\Users\\Angel\\Documents\\Spring 2021\\CSC 3380 - OOD\\Code\\sample1\\text\\taskList.txt";
+
+            TextReader rtxt = new StreamReader(Globals.path);
+            rtxt.ReadToEnd();
+            rtxt.Close();
+            
+        }
+
+        public static void TListW()
+        {
+            // STREAMWRITER CREATES A NEW FILE...
+            //using (StreamWriter sw = File.W(Globals.path))
+            //{
+            //    sw.WriteLine(input);
+            //    //Console.WriteLine("Added: " + input);
+            //}
+            TextWriter txt = new StreamWriter(Globals.path);
+            //txt.Write(txtTask.Text);
+           // txt.Write(output);
+            txt.Close();
+        }
+    }
+}
